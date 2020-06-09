7ca649f1dc0868fc45de48b056ab34cf94d0e4fd
Serhii Yakymenko <m.serhiiyakymenko@gmail.com>
2020-06-09 14:08:01 -0400

    Feat: added most all section

--- js/main.js
+++ js/main.js
@@ -39,6 +39,19 @@ $(".women-1__play").on("click", function onYouTubeIframeAPIReady() {
   });
 });
 
+var player2;
+$(".women-2__play").on("click", function onYouTubeIframeAPIReady() {
+  player2 = new YT.Player('players', {
+    height: '387',
+    width: '100%',
+    videoId: 'OPm_EDTrz7Y',
+    events: {
+      'onReady': videoPlayer,
+      // 'onStateChange': onPlayerStateChange
+    }
+  });
+});
+
 function videoPlayer(event) {
   // player.videoPlayer();
   event.target.playVideo();
